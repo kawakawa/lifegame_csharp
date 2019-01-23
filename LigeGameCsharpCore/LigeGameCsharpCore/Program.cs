1c4ac3008c05767e6aa5e766a3ef04ef8126dd73
TakuyaKawabe <rapidarrow2000@gmail.com>
2019-01-23 17:18:00 +0900

    LifeGameソリューション作成

---
+++ LigeGameCsharpCore/LigeGameCsharpCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LigeGameCsharpCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
